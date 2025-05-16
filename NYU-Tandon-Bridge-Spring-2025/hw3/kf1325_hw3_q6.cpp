f767d94485b021089c071c8501856a14c1c43413
krulci <einfolk.krulcifer@gmail.com>
2025-05-16 13:41:54 -0400

    Made correction to HW3 Q6

--- NYU-Tandon-Bridge-Spring-2025/hw3/kf1325_hw3_q6.cpp
+++ NYU-Tandon-Bridge-Spring-2025/hw3/kf1325_hw3_q6.cpp
@@ -5,21 +5,13 @@ using namespace std;
 
 int main()
 {
-	enum DayOfWeek
-	{
-		Mo, Tu, We, Th, Fr, Sa, Su
-	};
-
-	map<string, DayOfWeek> weekMap = { {"Mo", Mo}, {"Tu", Tu}, {"We", We}, {"Th", Th}, {"Fr", Fr}, {"Sa", Sa}, {"Su", Su} };
 	string dayOfWeek;
-	DayOfWeek dayOfWeekEnum;
 	double rate;
 	int hours, minutes, lengthOfCall, time;
 	char colon;
 
 	cout << "Please enter the day of the week read as one of the following two character string: Mo Tu We Th Fr Sa Su: " << endl;
 	cin >> dayOfWeek;
-	dayOfWeekEnum = weekMap[dayOfWeek];
 
 	cout << "Please enter the time the call started in 24-hour notation, so the time 1:30 P.M. is input as 13:30: " << endl;
 	cin >> hours >> colon >> minutes;
@@ -28,18 +20,13 @@ int main()
 	cout << "Please enter the length of the call in minutes: " << endl;
 	cin >> lengthOfCall;
 
-	switch (dayOfWeekEnum)
-	{
-		case Mo || Tu || We || Th || Fr:
-			if (time >= 8 * 60 && time < 18 * 60)
-				rate = 0.40;
-			else
-				rate = 0.25;
-			break;
-		default:
-			rate = 0.15;
-			break;
-	}
+	if (dayOfWeek == "Mo" || dayOfWeek == "Tu" || dayOfWeek == "We" || dayOfWeek == "Th" || dayOfWeek == "Fr")
+		if (time >= 8 * 60 && time < 18 * 60)
+			rate = 0.40;
+		else
+			rate = 0.25;
+	else
+		rate = 0.15;
 
 	cout << "The cost of the call is " << lengthOfCall * rate << endl;
 	return 0;
